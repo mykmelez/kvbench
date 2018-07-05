aeb775ec6346fe08202e9c3c1d7bbc6bdce216de
Myk Melez <myk@mykzilla.org>
2018-07-05 14:10:30 -0700

    introduce benchmark

--- src/bin/lmdb-write.rs
+++ src/bin/lmdb-write.rs
@@ -1,5 +1,9 @@
-extern crate tempdir;
+#![feature(test)]
+
 extern crate lmdb;
+extern crate rand;
+extern crate tempdir;
+extern crate test;
 
 use tempdir::TempDir;
 use lmdb::{
@@ -11,22 +15,82 @@ use lmdb::{
 
 
 fn main() {
-    let dir = TempDir::new("test").unwrap();
-    let env = Environment::new().open(dir.path()).unwrap();
-    let db = env.open_db(None).unwrap();
+}
 
-    let mut txn = env.begin_rw_txn().unwrap();
-    txn.put(db, b"key1", b"val1", WriteFlags::empty()).unwrap();
-    txn.put(db, b"key2", b"val2", WriteFlags::empty()).unwrap();
-    txn.put(db, b"key3", b"val3", WriteFlags::empty()).unwrap();
-    txn.commit().unwrap();
+#[cfg(test)]
+mod tests {
+    use ::{
+        Environment,
+        Error,
+        TempDir,
+        Transaction,
+        WriteFlags,
+    };
+    use rand::{Rng, XorShiftRng};
+    use test::{Bencher, black_box};
 
-    let mut txn = env.begin_rw_txn().unwrap();
-    assert_eq!(b"val1", txn.get(db, b"key1").unwrap());
-    assert_eq!(b"val2", txn.get(db, b"key2").unwrap());
-    assert_eq!(b"val3", txn.get(db, b"key3").unwrap());
-    assert_eq!(txn.get(db, b"key"), Err(Error::NotFound));
+    #[test]
+    fn test_get_put_del() {
+        let dir = TempDir::new("test").unwrap();
+        let env = Environment::new().open(dir.path()).unwrap();
+        let db = env.open_db(None).unwrap();
 
-    txn.del(db, b"key1", None).unwrap();
-    assert_eq!(txn.get(db, b"key1"), Err(Error::NotFound));
+        let mut txn = env.begin_rw_txn().unwrap();
+        txn.put(db, b"key1", b"val1", WriteFlags::empty()).unwrap();
+        txn.put(db, b"key2", b"val2", WriteFlags::empty()).unwrap();
+        txn.put(db, b"key3", b"val3", WriteFlags::empty()).unwrap();
+        txn.commit().unwrap();
+
+        let mut txn = env.begin_rw_txn().unwrap();
+        assert_eq!(b"val1", txn.get(db, b"key1").unwrap());
+        assert_eq!(b"val2", txn.get(db, b"key2").unwrap());
+        assert_eq!(b"val3", txn.get(db, b"key3").unwrap());
+        assert_eq!(txn.get(db, b"key"), Err(Error::NotFound));
+
+        txn.del(db, b"key1", None).unwrap();
+        assert_eq!(txn.get(db, b"key1"), Err(Error::NotFound));
+    }
+
+    pub fn get_key(n: u32) -> String {
+        format!("key{}", n)
+    }
+
+    pub fn get_data(n: u32) -> String {
+        format!("data{}", n)
+    }
+
+    pub fn setup_bench_db<'a>(num_rows: u32) -> (TempDir, Environment) {
+        let dir = TempDir::new("test").unwrap();
+        let env = Environment::new().open(dir.path()).unwrap();
+
+        {
+            let db = env.open_db(None).unwrap();
+            let mut txn = env.begin_rw_txn().unwrap();
+            for i in 0..num_rows {
+                txn.put(db, &get_key(i), &get_data(i), WriteFlags::empty()).unwrap();
+            }
+            txn.commit().unwrap();
+        }
+        (dir, env)
+    }
+
+    #[bench]
+    fn bench_get_rand(b: &mut Bencher) {
+        let n = 100u32;
+        let (_dir, env) = setup_bench_db(n);
+        let db = env.open_db(None).unwrap();
+        let txn = env.begin_ro_txn().unwrap();
+
+        let mut keys: Vec<String> = (0..n).map(|n| get_key(n)).collect();
+        XorShiftRng::new_unseeded().shuffle(&mut keys[..]);
+
+        b.iter(|| {
+            let mut i = 0usize;
+            for key in &keys {
+                i = i + txn.get(db, key).unwrap().len();
+            }
+            black_box(i);
+        });
+    }
+
 }
